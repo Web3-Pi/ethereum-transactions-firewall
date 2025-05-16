af7c0a86ff9fea987878026d404d8c986d9a51ff
Marcin Gordel <marcin.gordel@gmail.com>
2025-05-16 12:32:57 +0200

    Refactor and generalize alert component

--- src/firewall.ts
+++ src/firewall.ts
@@ -31,7 +31,7 @@ export class Firewall {
     this.app.get("/config.js", (req, res) => {
       res.setHeader("Content-Type", "application/javascript");
       res.send(
-        `window.__CONFIG = { WS_URL: "ws://${hostname}:${this.config.wssPort}", INTERACTIVE_MODE_TIMEOUT_SEC: ${this.config.interactiveModeTimeoutSec} };`,
+        `window.__CONFIG = { FIREWALL_MODE: "${this.config.mode}", WS_URL: "ws://${hostname}:${this.config.wssPort}", INTERACTIVE_MODE_TIMEOUT_SEC: ${this.config.interactiveModeTimeoutSec} };`,
       );
     });
 
@@ -39,11 +39,13 @@ export class Firewall {
   }
 
   private setupRoutes(): void {
-    const publicDir = path.join(__dirname, "../frontend/dist");
+    if (this.config.mode === "interactive") {
+      const publicDir = path.join(__dirname, "../frontend/dist");
 
-    this.app.get("/", (req, res) => {
-      res.sendFile(path.join(publicDir, "index.html"));
-    });
+      this.app.get("/", (req, res) => {
+        res.sendFile(path.join(publicDir, "index.html"));
+      });
+    }
 
     this.app.post("/api/reload", async (req, res) => {
       if (!this.proxy) {
@@ -101,11 +103,13 @@ export class Firewall {
     );
 
     this.app.listen(this.config.serverPort, () =>
-      this.proxy!.listen().then(() =>
-        this.logger.info(
-          `Transaction Firewall HTTP Server (to accept/reject transactions): http://${hostname}:${this.config.serverPort}`,
-        ),
-      ),
+      this.proxy!.listen().then(() => {
+        if (this.config.mode === "interactive") {
+          this.logger.info(
+            `Transaction Firewall HTTP Server (to accept/reject transactions): http://${hostname}:${this.config.serverPort}`,
+          );
+        }
+      }),
     );
 
     process.on("SIGINT", async () => {
